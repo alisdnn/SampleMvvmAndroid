b7007b98fa4e4bc525de74108e687202907c0a5a
Ali Sadeghian <aliosdn@gmail.com>
2024-08-10 15:01:11 -0400

    Add nav graph

--- app/src/main/java/com/alisdn/samplemvvmandroid/MainActivity.kt
+++ app/src/main/java/com/alisdn/samplemvvmandroid/MainActivity.kt
@@ -11,8 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.alisdn.samplemvvmandroid.ui.theme.SampleMvvmAndroidTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,28 +26,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             SampleMvvmAndroidTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    NavGraph()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SampleMvvmAndroidTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/alisdn/samplemvvmandroid/NavGraph.kt
@@ -0,0 +1,28 @@
+package com.alisdn.samplemvvmandroid
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.alisdn.samplemvvmandroid.domain.CatViewModel
+import com.alisdn.samplemvvmandroid.presentation.CatDetailScreen
+import com.alisdn.samplemvvmandroid.presentation.CatGrid
+
+@Composable
+fun NavGraph(startDestination: String = "cat_grid") {
+    val navController = rememberNavController()
+    val sharedViewModel: CatViewModel = hiltViewModel()
+
+    NavHost(navController = navController, startDestination = startDestination) {
+        composable("cat_grid") {
+            CatGrid(navController = navController, viewModel = sharedViewModel) { selectedCat ->
+                navController.navigate("cat_details/${selectedCat.id}")
+            }
+        }
+        composable("cat_details/{catId}") { backStackEntry ->
+            val catId = backStackEntry.arguments?.getString("catId")
+            CatDetailScreen(catId = catId, viewModel = sharedViewModel)
+        }
+    }
+}
